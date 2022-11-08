68869deab2ff1f114246eed728208b64e710cbb5
Pfeffimann18 <leon_gross@icloud.com>
2022-11-08 15:15:28 +0100

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+# i3 Config
+cp config ~/.config/i3
+
+
+# Picom
+cp picom.conf ~/picom.conf
+
+
+# Ranger Theme
+mkdir .config/ranger/
+mkdir .config/ranger/colorschemes
+cp default.py ~/.config/ranger/colorschemes
