c894243cb2d75eb367e8e1362e00a0487ccd1a5b
Pfeffimann18 <leon_gross@icloud.com>
2022-11-08 15:25:48 +0100

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
+# i3 Install
+sudo pacman -S i3-gaps i3lock xorg xfce4-settings nitrogen thunar firefox dmenu ttf-font-awesome polkit alsa-utils pulseaudio-alsa pulseaudioreb alacritty geany picom engrampa libsecret libgnome-keyring
+yay -S ly j4-dmenu-desktop bumblebee-status ulauncher
+
 # i3 Config
 cp config ~/.config/i3
 
 
 # Picom
 cp picom.conf ~/picom.conf
+
+
+# Alacritty
+cp alacritt.yml ~/.config
 
 
 # Ranger Theme
